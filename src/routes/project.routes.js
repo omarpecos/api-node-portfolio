ee71c8d369da82169acc2a7cbcab88c5050fe399
Omar Pecos <omarpecos@gmail.com>
2021-08-08 14:55:07 +0200

    [FIX] refactor project routes

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,86 +1,35 @@
 const { Router } = require('express');
-const { mongo } = require('mongoose');
-const { ProjectService } = require('../services');
-const { AuthenticationMiddleware } = require('../middlewares');
+
+const {
+  AuthenticationMiddleware,
+  projectMiddleware: middleware,
+} = require('../middlewares');
+const { ProjectController } = require('../controllers');
 
 const projectRouter = new Router();
 
-projectRouter.get('/', AuthenticationMiddleware, async (req, res) => {
-  const {
-    user: { _id: userId },
-  } = req;
+projectRouter.get(
+  '/',
+  AuthenticationMiddleware,
+  ProjectController.getAllProjects
+);
 
-  const projects = await ProjectService.getAllProjects(userId);
+projectRouter.post(
+  '/',
+  AuthenticationMiddleware,
+  ProjectController.createProject
+);
 
-  res.status(200).json({
-    status: 'success',
-    data: projects,
-  });
-});
+projectRouter.put(
+  '/:projectUuid',
+  [AuthenticationMiddleware, middleware.loadProject, middleware.canEditProject],
+  ProjectController.updateProject
+);
 
-projectRouter.post('/', AuthenticationMiddleware, async (req, res) => {
-  const {
-    user: { _id: userId },
-  } = req;
-  const { body: newProject } = req;
-
-  let project;
-
-  if (newProject._id == null) {
-    //creating new Project
-    newProject._id = mongo.ObjectId();
-    newProject.userId = userId;
-  } else {
-    // is editing existing project
-    project = await ProjectService.getOneProject(newProject._id);
-
-    if (!project) {
-      const err = new Error('Project not found');
-      err.status = 404;
-      throw err;
-    }
-
-    if (project.userId != userId) {
-      const err = new Error('Unathorized - This is not your project');
-      err.status = 403;
-      throw err;
-    }
-  }
-
-  project = await ProjectService.createOrUpdateProject(newProject);
-
-  res.status(200).json({
-    status: 'success',
-    data: project,
-  });
-});
-
-projectRouter.delete('/:id', AuthenticationMiddleware, async (req, res) => {
-  const {
-    user: { _id: userId },
-  } = req;
-  const { id } = req.params;
-
-  let project = await ProjectService.getOneProject(id);
-
-  if (!project) {
-    const err = new Error('Project not found');
-    err.status = 404;
-    throw err;
-  }
-
-  if (project.userId != userId) {
-    const err = new Error('Unathorized');
-    err.status = 403;
-    throw err;
-  }
-
-  project = await ProjectService.deleteProject(id);
-
-  res.status(200).json({
-    status: 'success',
-    data: project,
-  });
-});
+projectRouter.delete(
+  '/:projectUuid',
+  [AuthenticationMiddleware, middleware.loadProject, middleware.canEditProject],
+  ProjectController.deleteProject
+);
 
 module.exports = projectRouter;
